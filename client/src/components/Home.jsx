9bb128ece5476b35aac0c68514d3608f8f29c01e
mack69 <ghoshmayukh.janai@gmail.com>
2024-12-28 20:20:41 +0530

    LogReg Front End

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,7 @@ import '../components/CSS/home.css'
 function Home() {
   return (
     <>
+    <div className="homescreen">
     <div className='navbar'>
       {/* Logo Section */}
       <div className="logo">
@@ -13,22 +14,27 @@ function Home() {
       </div>
       {/* Menu Section */}
       <div className="menu">
-        <a href="#"><i className="fa-solid fa-house"></i> Home</a>
-        <a href="#"><i className="fa-solid fa-circle-info"></i> About</a>
-        <a href="#"><i className="fa-solid fa-envelope"></i> Contact</a>
+        <a href="#"><i className="fa-solid fa-house"></i> <p style={{color:"black"}}>Home</p></a>
+        <a href="#"><i className="fa-solid fa-circle-info"></i> <p style={{color:"black"}}>About</p></a>
+        <a href="#"><i className="fa-solid fa-envelope"></i> <p style={{color:"black"}}>Contact</p></a>
       </div>
       {/* Admin Section */}
       <div className="admin">
-        <a href="#" style={{ marginRight: "50px" }}> Admin </a>
+        <a href="#" style={{ marginRight: "50px",color:"black" }}>Admin</a>
       </div>
     </div>
-    
-    
-    
-    
+    <div className='description'>
+        {/* Description Section */}
+        <section className="Description-txt">
+          <span style={{ fontFamily: "Vast Shadow" }}>Socio-Sphere</span>
+          <h1>Smart Society Management System</h1>
+          <br />
+          <Link to='/Login'>Sign IN</Link>
+        </section>
+    </div>
+    </div>
     </>    
   )
 } 
-
 export default Home
 
